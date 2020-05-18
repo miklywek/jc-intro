2d7ee4dc3300a7de5061b7fe250aa61ddb60e7da
miklywek <miklywek@gmail.com>
2020-05-18 22:22:08 +0300

    fixed index.js runtime exception

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@ let greetings = 'World';
 alert(greetings);
 const message = 'Hello ' + greetings;
 alert(message);
-const userAge = parseFloat(prompt ('Enter your age'));
+const userAge = parseInt(prompt ('Enter your age'));
 alert ('Your age is ' + userAge);
- const nextAge = userAge, 1;
+ const nextAge = userAge + 1;
 alert(nextAge);
  if (userAge =>18) {
      alert('you are adult');
  } else {
-     alert('you are underage');
+     alert('you are underaged');
 
  }
