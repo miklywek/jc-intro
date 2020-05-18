ab18178ea34896d02d68cc65fd16b3f073a328cd
miklywek <miklywek@gmail.com>
2020-05-18 22:00:01 +0300

    problem

---
+++ js/index.js
@@ -0,0 +1,15 @@
+alert('Hello Miklusha');
+let greetings = 'World';
+alert(greetings);
+const message = 'Hello ' + greetings;
+alert(message);
+const userAge = parseFloat(prompt ('Enter your age'));
+alert ('Your age is ' + userAge);
+ const nextAge = userAge, 1;
+alert(nextAge);
+ if (userAge =>18) {
+     alert('you are adult');
+ } else {
+     alert('you are underage');
+
+ }
